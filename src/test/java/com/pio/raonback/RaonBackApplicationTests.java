6383f4a4e18ce6aef3453b7d57439c7625f93235
HYK <junhyeok001010@gmail.com>
2025-04-02 12:24:53 +0900

    프로젝트 초기화

---
+++ src/test/java/com/pio/raonback/RaonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pio.raonback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaonBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
