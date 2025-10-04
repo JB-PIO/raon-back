7b6b55d1268c5df1c888592c51efce43bddca8a1
HYK <junhyeok001010@gmail.com>
2025-10-04 15:51:10 +0900

    UTC 시간대 설정

--- src/main/java/com/pio/raonback/RaonBackApplication.java
+++ src/main/java/com/pio/raonback/RaonBackApplication.java
@@ -1,9 +1,12 @@
 package com.pio.raonback;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @ConfigurationPropertiesScan
@@ -12,6 +15,11 @@ public class RaonBackApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RaonBackApplication.class, args);
+  }
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 
 }
